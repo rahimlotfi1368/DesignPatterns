8f996a1d861e3754d6ecd555435e81480201f4b8
ra-lotfi <ra-lotfi@agri-bank.com>
2023-12-31 13:34:17 +0330

    Add project files.

---
+++ Implemention/Program.cs
@@ -0,0 +1,14 @@
+﻿using FactoryMethod;
+
+namespace Implemention
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryMethod.Factory factory = new FactoryMethod.GenericFactory<IronDoor>(); //new FactoryMethod.blacksmith();//Carpenter();
+            factory.CreatDoor();
+            Console.ReadKey();
+        }
+    }
+}
